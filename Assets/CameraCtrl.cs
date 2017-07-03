91db375747abc8acff34b3a8753867f7269ee873
Daniel Wilson <daniel_j_wilson@hotmail.com>
2017-07-03 12:45:57 -0400

    made camera follow player

---
+++ Assets/CameraCtrl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Camera ctrl.
+/// </summary>
+public class CameraCtrl : MonoBehaviour {
+
+	public Transform player;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3(player.position.x,player.position.y,transform.position.z);
+	}
+}
