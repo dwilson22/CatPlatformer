1b9542773758794dc7ce1e834e414d8a40bc957d
Daniel Wilson <daniel_j_wilson@hotmail.com>
2017-07-07 22:40:27 -0400

    added garbage collection below camera

---
+++ Assets/GarbageCtrl.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GarbageCtrl : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other){
+		Destroy (other.gameObject);
+	}
+}
