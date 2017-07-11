bea336e3a4ea3713d9f6d261d63038b492ce73f9
Daniel Wilson <daniel_j_wilson@hotmail.com>
2017-07-10 22:38:08 -0400

    added binary save functions, can now save and load game data

--- Assets/Scripts/GAMECtrl.cs
+++ Assets/Scripts/GAMECtrl.cs
@@ -2,15 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 public class GAMECtrl : MonoBehaviour {
 	public static GAMECtrl instance;
 	public float restartDelay;
+	public GameData data;
 
+	string dataFilePath;
+	BinaryFormatter bf;
 	void Awake(){
 		if (instance == null) {
 			instance = this;
 		}
+		bf = new BinaryFormatter();
+		dataFilePath = Application.persistentDataPath + "/game.dat";
+
+		Debug.Log (dataFilePath);
 	}
 	// Use this for initialization
 	void Start () {
@@ -19,7 +28,44 @@ public class GAMECtrl : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(Input.GetKeyDown(KeyCode.Escape)){
+			ResetData ();
+		}
+	}
+	public void SaveData(){
+		FileStream fs = new FileStream (dataFilePath, FileMode.Create);
+		bf.Serialize (fs, data);
+		fs.Close ();
+	}
+
+
+
+	public void LoadData(){
+		if(File.Exists(dataFilePath)){ 
+			FileStream fs = new FileStream (dataFilePath, FileMode.Open);
+			data = (GameData) bf.Deserialize (fs);
+			Debug.Log ("NUM OF COINS" + data.coinCount);
+			fs.Close();
+		}
+	}
+
+	void OnEnable(){
+		Debug.Log ("Loading");
+		LoadData ();
+
+	}
+
+	void OnDisable(){
+		Debug.Log ("Saving");
+		SaveData ();
+	}
+
+	void ResetData(){
+	FileStream fs = new FileStream (dataFilePath, FileMode.Create);
+		data.coinCount = 0;
+		bf.Serialize (fs, data);
+		fs.Close ();
+		Debug.Log ("Reset");
 	}
 	/// <summary>
 	/// Restarts the level when player dies.
