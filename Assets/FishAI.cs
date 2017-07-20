caf94d043f9c7d938e298a877bd70c5c5b07c23a
Daniel Wilson <daniel_j_wilson@hotmail.com>
2017-07-20 14:23:05 -0400

    added jumping fish enemy

---
+++ Assets/FishAI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishAI : MonoBehaviour {
+	public float jumpSpeed;
+
+	Rigidbody2D rb;
+	SpriteRenderer sr;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+		sr = GetComponent<SpriteRenderer> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (rb.velocity.y > 0) {
+			sr.flipY = false;
+		} else {
+			sr.flipY = true;
+		}
+	}
+
+	public void FishJump(){
+		rb.AddForce (new Vector2 (0, jumpSpeed));
+	}
+}
---
+++ Assets/FishJumperCtrl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishJumperCtrl : MonoBehaviour {
+	public float jumpDelay;
+	public GameObject fish;
+	FishAI fai;
+	Rigidbody2D rb;
+	void Start(){
+		rb = fish.GetComponent<Rigidbody2D> ();
+		fai = fish.GetComponent<FishAI> ();
+	}
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.gameObject.CompareTag ("Enemy")) {
+			
+			rb.isKinematic = true;
+			rb.velocity = Vector2.zero;
+			Invoke ("CallFishJump", jumpDelay);
+		}
+	}
+
+	void CallFishJump(){
+		rb.isKinematic = false;
+		fai.FishJump ();
+	}
+}
